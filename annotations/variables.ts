41e24dc8ae93c5059eb1c86288878a567059959f
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-03-23 16:37:19 -0700

    annotations with simple types

---
+++ annotations/variables.ts
@@ -0,0 +1,10 @@
+let oranges: number = 5;
+let island: string = 'deserted';
+let hasName: boolean = true;
+
+//value with a name identical to it's type
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
