61b262d8850cdaf0616e8c0aa5d710958ccce8f5
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-03-24 20:28:33 -0700

    annotations for anonymous functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -2,7 +2,6 @@ const add = (a: number, b: number): number => {
   // you have to have a return value!
   return a + b;
 };
-
 // typescript will return the correct type value, 
 //but it doesn't care about the logic inside of the function
 
@@ -12,3 +11,11 @@ const subtract = (a: number, b: number) => {
 };
 // typescript inferes that since we're not returning anything it's void
 // we made a mistake, but typescript didn't tell us we did
+
+function divide(a: number, b: number): number {
+  return a / b;
+};
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+};
