68fc1bb6472186a0ed3c48efd0ffd960c20d679a
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-03-24 21:35:12 -0700

    annotating with pop, push, and map methods.

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -1,9 +1,23 @@
-const animalCrossing = ['wild world', 'new leaf', 'pocket camp', 'new horizons'];
+const animalCrossingGames = [
+  'wild world',
+  'new leaf',
+  'pocket camp',
+  'new horizons'
+];
 const dates = [new Date(), new Date()];
 
-const gamesByConsole = [
-  ['ds'],
-  ['3ds'],
-  ['mobile'],
-  ['switch']
-];
+const gamesByConsole = [['ds'], ['3ds'], ['mobile'], ['switch']];
+
+const animalCrossingConsole: string[][] = [];
+
+// Help with inference when extracting values
+const animalCrossing = animalCrossingGames[0];
+const myGame = animalCrossingGames.pop();
+
+// Prevent incompatible values
+animalCrossingGames.push(1000);
+
+// Help with 'map'
+animalCrossingGames.map((animalCrossing: string): string => {
+  return animalCrossing.toUpperCase();
+});
