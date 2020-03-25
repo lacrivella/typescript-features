2cf4d0fd88bef959620e7b31daa222e99c118889
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-03-24 20:14:47 -0700

    inference around functions and how typescript works:

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,4 +1,14 @@
 const add = (a: number, b: number): number => {
+  // you have to have a return value!
   return a + b;
 };
-// you have to have a return value!
+
+// typescript will return the correct type value, 
+//but it doesn't care about the logic inside of the function
+
+
+const subtract = (a: number, b: number) => {
+  a-b; 
+};
+// typescript inferes that since we're not returning anything it's void
+// we made a mistake, but typescript didn't tell us we did
