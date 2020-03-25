57a0dc4e339827ed81780b26a1fc6cbd6edbc15b
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-03-24 20:35:05 -0700

    annotating functions with errors, and voids

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -19,3 +19,22 @@ function divide(a: number, b: number): number {
 const multiply = function(a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+// having void as a type says it's not going to return anything as a value
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+// we will never return anything. 
+// instead we leave the function early after throwing an error
+
+const commonThrowError= (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
+//annotate to what you would return if no error
