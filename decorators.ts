249f63e6ee679616540a912e9239f63076197970
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-07-14 17:41:53 -0700

    created a function factory for the decorator so my error message can be customized for whatever class it is used in

--- decorators.ts
+++ decorators.ts
@@ -5,22 +5,24 @@ class Boat {
     return `This boat is painted ${this.color}`;
   }
 
-  @logError
+  @logError('Uh-oh! Boat hit an iceberg.')
   pilot(): void {
     throw new Error();
     console.log('swish swash');
   }
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value;
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
 
-  desc.value = function () {
-    try {
-      method();
-    } catch (err) {
-      console.log('Oops! Boat hit an iceberg.');
-    }
+    desc.value = function () {
+      try {
+        method();
+      } catch (err) {
+        console.log(errorMessage);
+      }
+    };
   };
 }
 
